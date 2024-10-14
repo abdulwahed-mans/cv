400c677b04f381182894a7e1363c10a5ec505acc
Abdulwahed Mansour <153433760+abdulwahed-mans@users.noreply.github.com>
2024-10-14 17:30:35 +0200

    add link to home

--- my-cv/app/contact/page.js
+++ my-cv/app/contact/page.js
@@ -1,8 +1,20 @@
+import Link from "next/link";
+
 export default function Contact() {
   return (
-    <div className="p-8">
-      <h1 className="text-4xl font-bold">Contact Me</h1>
-      <p className="mt-4">You can reach me at: abdulwahed.mansour@gmail.com</p>
-    </div>
+    <>
+      <div className="p-8">
+        <h1 className="text-4xl font-bold">Contact Me</h1>
+        <p className="mt-4">
+          You can reach me at: abdulwahed.mansour@gmail.com
+        </p>
+      </div>
+      // home page link
+      <div className="p-8">
+        <Link href="/">
+          <h2 className="text-blue-600">Go back to home</h2>
+        </Link>
+      </div>
+    </>
   );
 }
--- my-cv/app/page.js
+++ my-cv/app/page.js
@@ -1,10 +1,17 @@
 // Example in app/page.js
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <h1 className="text-3xl font-bold text-blue-600">
         Welcome to my CV page!
       </h1>
+      <Link href="/contact">
+        <h1 className="text-3xl font-bold text-red-600">contact</h1>
+      </Link>
     </div>
   );
 }
+
+//return <Link href="/dashboard">Dashboard</Link>
